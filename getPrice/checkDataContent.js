25361cc48d22ec5d8602db3f13d3da1ed4cc8f2d
楊竣宇\yang <ch20026103@yahoo.com.tw>
2021-08-10 23:52:06 +0800

    Add checkDataContent and replace all data.json

---
+++ getPrice/checkDataContent.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+let rawdata = fs.readFileSync("./aa.json");
+let jsonData = JSON.parse(rawdata);
+let keys = Object.keys(jsonData)
+
+let error = []
+keys.forEach(key => {
+    for (let i = 0; i < jsonData[key].length; i++) {
+        const element = jsonData[key][i];
+        if(
+            !element.hasOwnProperty('stockAgentMainPower') ||
+            !element.hasOwnProperty('c') ||
+            !element.hasOwnProperty('v') ||
+            !element.hasOwnProperty('o') ||
+            !element.hasOwnProperty('l') ||
+            !element.hasOwnProperty('h') ||
+            !element.hasOwnProperty('skp5') ||
+            !element.hasOwnProperty('sumForeignNoDealer') ||
+            !element.hasOwnProperty('sumING')
+        ){
+            error.push(key)
+            break;
+        }
+    }
+});
+console.log(error)
