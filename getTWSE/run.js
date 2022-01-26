3e8701683209a9b1d3ee1e13e734eb39e01919c9
楊竣宇\yang <ch20026103@yahoo.com.tw>
2022-01-26 22:17:29 +0800

    get data(2022/01/26)

--- getTWSE/run.js
+++ getTWSE/run.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { Ma } = require("@ch20026103/anysis");
+const { Ma, Williams } = require("@ch20026103/anysis");
 let twseJsonData = fs.readFileSync("./datas/TWSE/data.json");
 let twseData = JSON.parse(twseJsonData);
 
@@ -7,31 +7,42 @@ let epsJsonData = fs.readFileSync("./datas/Eps/data.json");
 let epsData = JSON.parse(epsJsonData).dataList;
 
 let ma = new Ma();
+let williams = new Williams();
 
 /* bolling */
-// let purchaseList = [];
-// epsData.forEach((stockId) => {
-//   let stockData = twseData[stockId];
-//   if (!stockData) return;
-//   stockData = ma.getBoll(stockData);
+let purchaseList1 = [];
+epsData.forEach((stockId) => {
+  let stockData = twseData[stockId];
+  if (!stockData) return;
+  let maData = ma.getMa10(stockData.slice(-11));
+  let williamsData = williams.getAllWillams(stockData);
 
-//   let length = stockData.length;
-//   if (
-//     (stockData[length - 1]["sumING"]>0 || stockData[length - 1]["sumForeignNoDealer"]>0) &&
-//     stockData[length - 1]["bollLb"] < stockData[length - 1]["c"] &&
-//     stockData[length - 2]["bollLb"] < stockData[length - 2]["c"] &&
-//     (stockData[length - 3]["bollLb"] > stockData[length - 3]["c"] ||
-//       stockData[length - 4]["bollLb"] > stockData[length - 4]["c"])
-//   ) {
-//     purchaseList.push({
-//       date: stockData[length - 1]["t"],
-//       price: stockData[length - 1]["c"],
-//       ING: stockData[length - 1]["sumING"],
-//       [stockId]: stockData[length - 1]["name"],
-//     });
-//   }
-// });
-// console.log(purchaseList);
+  let length = stockData.length;
+  if (
+    stockData[length - 1]["h"] > stockData[length - 2]["h"] &&
+    stockData[length - 1]["l"] > stockData[length - 2]["l"] &&
+    (williamsData[williamsData.length - 2].williams9 < -80 ||
+      williamsData[williamsData.length - 3].williams9 < -80) &&
+    (williamsData[williamsData.length - 2].williams18 < -80 ||
+      williamsData[williamsData.length - 3].williams18 < -80) &&
+    stockData[length - 1]["c"] > maData[maData.length - 1]["ma10"]
+  ) {
+    purchaseList1.push({
+      date: stockData[length - 1]["t"],
+      price: stockData[length - 1]["c"],
+      I: stockData[length - 1]["sumING"],
+      [stockId]: stockData[length - 1]["name"],
+      F: stockData[length - 1]["sumForeignNoDealer"],
+    });
+  }
+});
+console.log(
+  "Method: K線反轉\n",
+  "----------------------------\n",
+  purchaseList1,
+  "\n",
+  "----------------------------\n"
+);
 
 /* stock */
 let purchaseList = [];
@@ -44,14 +55,22 @@ epsData.forEach((stockId) => {
   if (
     stockData[length - 1]["sumING"] > 100 &&
     stockData[length - 2]["sumING"] > 100 &&
-    stockData[length - 3]["sumING"] > 10
+    stockData[length - 3]["sumING"] > 100
   ) {
     purchaseList.push({
       date: stockData[length - 1]["t"],
       price: stockData[length - 1]["c"],
-      ING: stockData[length - 1]["sumING"],
+      I: stockData[length - 1]["sumING"],
+      F: stockData[length - 1]["sumForeignNoDealer"],
       [stockId]: stockData[length - 1]["name"],
     });
   }
 });
-console.log(purchaseList);
+console.log(
+  "Method: 投信買進\n",
+  "----------------------------\n",
+  purchaseList,
+  "\n",
+  "----------------------------\n"
+);
+
